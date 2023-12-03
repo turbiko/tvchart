dbb6f8a222f4b2e77e3dbfe490b1f2bee856c57a
Andriy Voznyuk <andreyv@ukr.net>
2023-12-03 19:46:48 +0100

    db configuration changed

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -8,13 +8,28 @@ DEBUG = True
 load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1p-plpfl!0v)3(3iob99#%(x@kf$0i#yuk=&&y7=diok16m7d^"
+
+SECRET_KEY = os.environ.get('SECRET_KEY', "dev-django-insecure-1p-plpfl!0v)3(3iob99#%(x@kf$0i#yuk=&&y7=diok16m7d^")
+print(f"{SECRET_KEY=}")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASE_NAME = os.environ.get('DATABASE_NAME')
+DATABASE_USER = os.environ.get('DATABASE_USER')
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+DATABASE_HOST = os.environ.get('DATABASE_HOST')
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
 
 try:
     from .local import *
--- core/settings/production.py
+++ core/settings/production.py
@@ -10,6 +10,22 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 load_dotenv()
 
+SECRET_KEY = os.environ.get('SECRET_KEY', "django-production-1p-plpfl!0v)3(3iob99#%(x@kf$0i#yuk=&&y7=diok16m7d^")
+print(f"{SECRET_KEY=}")
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASE_NAME = os.environ.get('DATABASE_NAME')
+DATABASE_USER = os.environ.get('DATABASE_USER')
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+DATABASE_HOST = os.environ.get('DATABASE_HOST')
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
 
 try:
     from .local import *
